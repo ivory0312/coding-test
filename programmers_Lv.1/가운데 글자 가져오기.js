a11e628eaffe7b632f50d03f0b2f4c902ad9db79
ivory0312 <63035523+ivory0312@users.noreply.github.com>
2021-04-29 12:05:23 +0900

    Create 가운데 글자 가져오기.js

---
+++ programmers_Lv.1/가운데 글자 가져오기.js
@@ -0,0 +1,16 @@
+function solution(s) {
+  var answer = '';
+  var arr = [];
+  const l = s.length;
+  const a = s.split('');
+  
+  if (l % 2 === 0) {
+      answer = s.slice(l / 2 - 1, l / 2 + 1);
+  }
+  else {
+      answer = s[parseInt(l / 2)];
+  }
+      
+  console.log(a);
+  return answer;
+}
